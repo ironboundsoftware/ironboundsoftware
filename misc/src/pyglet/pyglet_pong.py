be75baf9dcde54761f2655ce23ebd6fe2d9341bc
nloadholtes <nloadholtes@84addc56-6826-0410-adfa-332040e269ca>
2007-09-09 18:31:42 +0000

    -Added in the "AI" for the computer player. Pretty crappy, but it works.

--- misc/src/pyglet/pyglet_pong.py
+++ misc/src/pyglet/pyglet_pong.py
@@ -16,11 +16,14 @@ from random import randint
 
 win = window.Window(600, 600)
 img = image.load('red_paddle.png')
+comp_paddle_img = image.load('red_paddle.png')
 ball_img = image.load('ball.png')
 ft = font.load('Arial', 36)
 
 paddle_x = 0
 paddle_y = 0
+comp_paddle_x = 580
+comp_paddle_y = 0
 ball_x = 300
 ball_y = 300
 ball_vx = -5
@@ -39,13 +42,15 @@ win.on_mouse_drag = moveMouse
 win.set_mouse_visible(False)
 
 def moveBall():
-	global ball_x, ball_y, ball_vx, ball_vy, width, height, paddle_x, paddle_y, score_player, score_comp
+	global ball_x, ball_y, ball_vx, ball_vy, width, height, paddle_x, paddle_y, score_player, score_comp, comp_paddle_x, comp_paddle_y
 	ball_x += ball_vx
 	ball_y += ball_vy
 
-	if ball_x > width:
+	if ball_x-10 in range(comp_paddle_x, comp_paddle_x +10) and ball_y+10 in range(comp_paddle_y, comp_paddle_y+40):
 		ball_vx *= -1
-		ball_x += ball_vx *2
+		ball_vy *= -1
+		ball_x += ball_vx *(1 + randint(2,5))
+		ball_y += ball_vy *(1 + randint(2,5))
 		return
 	if ball_x+10 in range(paddle_x, paddle_x+10) and ball_y+10 in range(paddle_y, paddle_y+40):
 		ball_vx *= -1
@@ -59,6 +64,13 @@ def moveBall():
 		ball_x = randint(width/2, width)
 		ball_y = randint(height/2, height)
 		return
+	if ball_x-10 >= width:
+		print "You scoreed!"
+		score_player += 1
+		ball_x = randint(width/2, width)
+		ball_y = randint(height/2, height)
+		return
+		
 	if ball_y >= height or  ball_y <= 0:
 		ball_vy *= -1
 		ball_y += ball_vy *2
@@ -68,14 +80,23 @@ def printScore(you, me):
 	s =  'Player: '+str(you)+'        Computer:'+str(me)
 	text = font.Text(ft,s, 0, height-40)
 	text.draw()
+	
+def moveCompPaddle():
+	global ball_x, ball_y, comp_paddle_x, comp_paddle_y
+	if ball_y < comp_paddle_y:
+		comp_paddle_y -= 4
+	if ball_y > comp_paddle_y:
+		comp_paddle_y += 4
 
 #
 # Event loop
 while not win.has_exit:
 	win.dispatch_events()
 	moveBall()
+	moveCompPaddle()
 	win.clear()
 	printScore(score_player, score_comp)
 	img.blit(paddle_x, paddle_y)
+	comp_paddle_img.blit(comp_paddle_x, comp_paddle_y)
 	ball_img.blit(ball_x, ball_y)
 	win.flip()
