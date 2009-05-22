1fe174a603b03e04eee9b40f4dc71472223d134d
nloadholtes <nloadholtes@84addc56-6826-0410-adfa-332040e269ca>
2009-05-22 03:09:19 +0000

    -Sloooooowly working on this.

--- trunk/SatId/tests/SatId/SGP4Test.py
+++ trunk/SatId/tests/SatId/SGP4Test.py
@@ -4,19 +4,20 @@ Created on May 14, 2009
 @author: njl
 '''
 import unittest
+import SatId
 from SatId import SGP
 
 class Test(unittest.TestCase):
 
 
-	def t3estSample(self):
+	def testSample(self):
 		s = SGP.SGP4(None,3)
 		self.assertNotEqual((None, None, None), s)
 
 	def testWTF(self):
 		import SatId
 		print "Looking at SatId"
-		print dir(SatId)
+
 		
 if __name__ == "__main__":
 	#import sys;sys.argv = ['', 'Test.testSample']
