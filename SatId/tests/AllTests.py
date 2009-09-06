42912e142cb5b22716042af0faae61d65bbb788c
nloadholtes <nloadholtes@84addc56-6826-0410-adfa-332040e269ca>
2009-09-06 19:40:46 +0000

    moving directory names (unit tests were causing a circular import problem that Python apparently can't handle)

--- SatId/tests/AllTests.py
+++ SatId/tests/AllTests.py
@@ -6,13 +6,13 @@ Created on Sep 5, 2009
 
 import unittest
 import MPCTest, TLETest
-#import SatId.AllSatIdTests
+import TSatId.AllSatIdTests
 
 if __name__ == '__main__':
 #	alltests = unittest.TestSuite()
 #	alltests.addTest(MPCTest.suite())
-	alltests = unittest.TestSuite([MPCTest.suite(), TLETest.suite(),])
-#								SatId.AllSatIdTests.suite()])
+	alltests = unittest.TestSuite([MPCTest.suite(), TLETest.suite(),
+								TSatId.AllSatIdTests.suite()])
 	runner = unittest.TextTestRunner()
-	runner.run(alltests)
+	runner.run(alltests) 
 	
